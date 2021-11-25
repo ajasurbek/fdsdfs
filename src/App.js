da75621a1fd59d472f37b3ae503f98773f65e008
Jasurbek <70360694+ajasurbek@users.noreply.github.com>
2021-11-25 22:30:08 +0500

    asd

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+import {
+  Home, 
+  Movies,
+  SingleMovie,
+  SingleShow,
+  TvShow,
+  SingleTopRated,
+  TopRated,
+  UpComing,
+  SingleUpComing,
+  SingleActor
+} from './pages'
+import Error from './components/ErrorPage/Error'
+import Search from './components/Search/Search'
 
-import Header from './containers/header/header'
+import './assets/styles/main.scss'
+import Header from './containers/header'
 
 function App() {
   return (
@@ -8,11 +23,23 @@ function App() {
     <Router>
       <Header />
     <Switch>
-   
+    <Route exact path='/' component={Home} /> 
+    <Route  path='/movies' component={Movies} /> 
+    <Route  path='/latestMovies' component={TopRated} /> 
+    <Route  path='/tvshows' component={TvShow} /> 
+    <Route  path='/upcoming' component={UpComing} /> 
+    <Route  path='/upcoming/:id' component={SingleUpComing} /> 
+    <Route  path='/movie/:id' component={SingleMovie} />
+    <Route  path='/movie/actor/:id' component={SingleActor} />
+    <Route  path='/tvshow/:id' component={SingleShow} /> 
+    <Route  path='/latestMovie/:id' component={SingleTopRated} /> 
+    <Route  path="/search/:searchQuery" component={Search} />
+    <Route  path='*' component={Error} />
     </Switch>
     </Router>
     </>
     );
   }
-
+  
   export default App;
+  
